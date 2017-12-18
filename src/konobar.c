c48337fabd930a690e328827f4c2d2cf411a0a0a
Libido <leonardovlibido@gmail.com>
2017-12-19 00:56:10 +0100

    zapoceta implementacija funkcionalnosti konobara

--- src/konobar.c
+++ src/konobar.c
@@ -59,7 +59,7 @@ int prijaviKonobara( MYSQL *connection )
 		else{
 			titleScreen();
 			printf("Dobrodosli %s.\n", bufferName);
-			printf("Unesite bilo koji karakter da biste nastavili\n");
+            pressAnyKeyToContinue();
 			validanUnos = 1;
 			idKonobara = atoi(row[0]);
 			// printf("Ovo je id konobara: %d\n", idKorisnika);
@@ -73,27 +73,48 @@ int prijaviKonobara( MYSQL *connection )
 void konobarAkcije( MYSQL *connection, int idKonobara )
 {
     int running = 1;
-	while ( running ){
+
+	titleScreen();
+    int idKafica = izaberiKafic(connection, idKonobara);
+    pressAnyKeyToContinue();
+
+    while ( running ){
 		titleScreen();
         printf("\nIzaberite koju akciju zelite da izvrsite:\n");
-        printf("0. Odjavi se\n");
-        printf("1. Izlistaj rezervacije po kaficu.\n");
-        printf("2. Obradi rezervaciju na cekanju.\n");
-        printf("3. Promeni status rezervacija koje su bile danas.\n");
+        printf("0. Odjavi se.\n");
+        printf("1. Uloguj se u drugi kafic.\n");
+        printf("2. Izlistaj rezervacije na cekanju.\n");
+        printf("3. Izlistaj slobodne stolove.\n");
+        printf("4. Obradi rezervaciju na cekanju.\n");
+        printf("5. Promeni status rezervacija koje su bile danas.\n");
 		printPrompt();
         scanf("%d", &running);
 		switch ( running ){
 			case 0:
-			    printf("Dovidjenja.\n");
+			    titleScreen();
+			    printf("Uspesno ste se odjavili.\n");
+                pressAnyKeyToContinue();
 				break;
 			case 1:
-                //TODO
+                titleScreen();
+                idKafica = izaberiKafic(connection, idKonobara);
+                pressAnyKeyToContinue();
 				break;
 			case 2:
-                //TODO
+                titleScreen();
+                izlistajRezervacijePoKaficu(connection, idKonobara);
+                pressAnyKeyToContinue();
 				break;
-			case 3:
-                //TODO
+            case 3:
+                titleScreen();
+                izlistajSlobodneStolove(connection, idKafica);
+                pressAnyKeyToContinue();
+				break;
+
+			case 4:
+                titleScreen();
+                obradiRezervacijuNaCekanju(connection, idKonobara, idKafica);
+                pressAnyKeyToContinue();
 				break;
             default:
                 printf("Los ulaz. Unesite redni broj opcije koju zelite da izaberete\n");
@@ -102,4 +123,105 @@ void konobarAkcije( MYSQL *connection, int idKonobara )
 	}
 }
 
+
+int izaberiKafic( MYSQL *connection, int idKonobara)
+{
+
+    MYSQL_RES *result;
+    char query[QUERY_SIZE];
+    int idKafica;
+
+    printf("Zaposleni ste u narednim kaficima:\n\n");
+    sprintf (query,
+            "select idKafic, ime, ulica, brojUlice\
+            from zaposlen z join Kafic k on z.Kafic_idKafic=k.idKafic\
+            where z.Konobar_idKonobar=%d", idKonobara);
+
+    /* Pokusava se sa izvrsavanjem upita. */
+    if (mysql_query (connection, query) != 0)
+        error_fatal ("Error in query %s\n", mysql_error (connection));
+
+    /* Preuzima se result. */
+    result = mysql_store_result (connection);
+    print_result(result, 0);
+    mysql_free_result (result);
+
+    printf("Unesite id Kafica za koji zelite da se ulogujete:\n");
+	printPrompt();
+    scanf("%d", &idKafica);
+
+}
+
+
+
+void izlistajRezervacijePoKaficu( MYSQL *connection, int idKafica)
+{
+    MYSQL_RES *result;
+    char query[QUERY_SIZE];
+
+    sprintf (query,
+            "select idRezervacije, datum, vreme, brLjudi, k.ime as imeKafica, status \
+            from Rezervacije r join Kafic k on r.Kafic_idKafic=k.idKafic \
+            where r.Kafic_idKafic=%d and status=\"na cekanju\"", idKafica);
+
+    /* Pokusava se sa izvrsavanjem upita. */
+    if (mysql_query (connection, query) != 0)
+        error_fatal ("Error in query %s\n", mysql_error (connection));
+
+    /* Preuzima se result. */
+    result = mysql_store_result (connection);
+    print_result(result, 0);
+    mysql_free_result (result);
+}
+
+
+void izlistajSlobodneStolove( MYSQL *connection, int idKafica)
+{
+    MYSQL_RES *result;
+    char query[QUERY_SIZE];
+
+    sprintf (query,
+            "select redniBroj, brojMesta\
+            from Sto\
+            where Kafic_idKafic=%d and trenutnoStanje=\"slobodno\"", idKafica);
+
+    /* Pokusava se sa izvrsavanjem upita. */
+    if (mysql_query (connection, query) != 0)
+        error_fatal ("Error in query %s\n", mysql_error (connection));
+
+    /* Preuzima se result. */
+    result = mysql_store_result (connection);
+    print_result(result, 0);
+    mysql_free_result (result);
+
+}
+
+
+void obradiRezervacijuNaCekanju( MYSQL *connection, int idKonobara, int idKafica)
+{
+    MYSQL_RES *result;
+    char query[QUERY_SIZE];
+    int idRezervacije;
+
+    izlistajRezervacijePoKaficu( connection, idKafica);
+
+    printf("Unesite id Rezervacije koju zelite da obradite");
+    printPrompt();
+    scanf("%d", &idRezervacije);
+
+    char* chooseAnotherTable[QUERY_SIZE];
+    chooseTable[0] = 'y';
+
+    while(chooseTable[0] == 'y' || chooseTable[0] =='Y'){
+        izlistajSlobodneStolove();
+        printf("Izaberite koji sto zelite da rezervisete\n");
+        printf("Uspesno ste rezervisali sto\n");
+        //TODO CODE
+        printf("Zelite li da rezervisete jos neki sto za datu rezervaciju?(Y/N)\n");
+        printPrompt();
+        scanf("%d", &chooseAnotherTable);
+    }
+
+    return;
+}
 
--- src/konobar.h
+++ src/konobar.h
@@ -6,6 +6,10 @@
 
 int prijaviKonobara( MYSQL *connection );
 void konobarAkcije( MYSQL *connection, int idKonobara );
+int izaberiKafic( MYSQL *connection, int idKonobara);
+void izlistajRezervacijePoKaficu( MYSQL *connection, int idKafica);
+void obradiRezervacijuNaCekanju( MYSQL *connection, int idKonobara, int idKafica);
+void izlistajSlobodneStolove( MYSQL *connection, int idKafica);
 
 #endif
 
